6664c39e3d7344f67df9f105030df61d92a4f7cb
Fiona Bianchi <fionahiklasdwp@gmail.com>
2015-06-24 17:08:22 +0100

    More tests for mustache templates

--- test/test_mustache_template.rb
+++ test/test_mustache_template.rb
@@ -17,6 +17,10 @@ class RCMMustacheTemplateTest < Test::Unit::TestCase
   JSON_FILE = 'test/client.post.test.json'
   FULL_TEMPLATE_FILE = 'conf/email-template.mustache'
   SIMPLE_DOT_TEMPLATE = 'test/dot-notation.mustache'
+  DEEP_DOT_TEMPLATE = 'test/dot-notation-deep.mustache'
+
+  SIMPLE_DOT_RESULT = "Hello Terry Pratchett!\nHow are you today?"
+  DEEP_DOT_RESULT = "Hello Terry Pratchett!\nHow are you today string?"
 
   def self.read_json
     @@log.debug('Reading file ...')
@@ -41,6 +45,7 @@ class RCMMustacheTemplateTest < Test::Unit::TestCase
     assert(mustache.template_file == FULL_TEMPLATE_FILE, 'Template file wasn\'t set to the correct value')
   end
 
+
   def test_dot_notation
     mustache = Mustache.new
     mustache.template_file = SIMPLE_DOT_TEMPLATE
@@ -58,10 +63,37 @@ class RCMMustacheTemplateTest < Test::Unit::TestCase
     output = mustache.render
 
     assert(output != nil, 'Didn\'t get any output')
-    @@log.debug('Got output from template: "%s"', output)
+    @@log.debug('Got output from template: "%s", expected: "%s"', output, SIMPLE_DOT_RESULT)
+
+    assert(output == SIMPLE_DOT_RESULT, 'Didn\'t get correct result')
   end
 
 
+  def test_dot_notation_deep
+    mustache = Mustache.new
+    mustache.template_file = DEEP_DOT_TEMPLATE
+
+    assert(mustache.template_file == DEEP_DOT_TEMPLATE, 'Template file wasn\'t set to the correct value')
+
+    data_hash = {
+        name: 'Terry Pratchett',
+        date: {
+            when: {
+                number: {
+                    string: 'today string'
+                }
+            }
+        }
+    }
+
+    mustache[:data] = data_hash
+    output = mustache.render
+
+    assert(output != nil, 'Didn\'t get any output')
+    @@log.debug('Got output from template: "%s" expected: "%s"', output, DEEP_DOT_RESULT)
+
+    assert(output == DEEP_DOT_RESULT, 'Didn\'t get correct result')
+  end
 
 end
 
